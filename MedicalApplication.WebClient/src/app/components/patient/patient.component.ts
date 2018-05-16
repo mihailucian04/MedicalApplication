986acc25782c9197a1347c6983cfffd385209ede
Gavris <antonio.gavris@sap.com>
2018-05-16 18:12:39 +0300

    Get patients

--- MedicalApplication.WebClient/src/app/components/patient/patient.component.ts
+++ MedicalApplication.WebClient/src/app/components/patient/patient.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material';
+import { PatientService } from '../../services/patient.service';
+import * as X2JS from 'x2js';
 
 @Component({
   selector: 'app-patient',
@@ -15,9 +17,22 @@ export class PatientComponent implements OnInit {
     filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
     this.dataSource.filter = filterValue;
   }
-  constructor() { }
+  constructor(private service: PatientService) { }
 
   ngOnInit() {
+    console.log('in ngoninit');
+    this.service.getPatientData()
+      .subscribe(
+        (result: Element[]) => {
+
+
+          //let x2js = new X2JS();
+          //console.log('json '+ (x2js.xml2js(result.toString())));
+          //let jsonResult: Element[] = JSON.parse(x2js.xml2js(result.toString()));
+          //console.log(jsonResult);
+          this.dataSource = new MatTableDataSource(result);
+        }
+      );
   }
 
 }
@@ -33,8 +48,8 @@ export interface Element {
 }
 
 const ELEMENT_DATA: Element[] = [
-  {position: 1, FirstName: 'Valentin', LastName:"Birac",CNP: "1930202394021", Sex:1,Address:"Str.Macinului"},
-  {position: 2, FirstName: 'Mariana', LastName:"Gacinovic",CNP: "2830102394021", Sex:2,Address:"Str.Pinului"},
+  {position: 1, FirstName: 'HardcodedFirstName', LastName:" ",CNP: "1930202394021", Sex:1,Address:"Str.Macinului"},
+  {position: 2, FirstName: 'HardcodedFirstName1', LastName:"Gacinovic",CNP: "2830102394021", Sex:2,Address:"Str.Pinului"},
   {position: 3, FirstName: 'Alina', LastName:"Mergea",CNP: "2730202394021", Sex:2,Address:"Str.Victor Ansac"},
   {position: 4, FirstName: 'Gheorghe', LastName:"Tecau",CNP: "1880502394021", Sex:1,Address:"Str.Bulevardul Macului"},
   {position: 5, FirstName: 'Ion', LastName:"Stanciu",CNP: "1730202394021", Sex:1,Address:"Str.Mihai Eminescu"},
--- MedicalApplication.WebClient/src/app/services/patient.service.ts
+++ MedicalApplication.WebClient/src/app/services/patient.service.ts
@@ -4,10 +4,14 @@ import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import { Patient } from '../components/patient/patient.model';
 
-
 @Injectable()
 export class PatientService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  public getPatientData() {
+    console.log('in service');
+    return this.httpClient.get('http://medappapi.azurewebsites.net/api/patient/get-patients-by-medic/FE2252DD-F551-E811-80C6-000D3AB15E2F');
+  }
 
 }
