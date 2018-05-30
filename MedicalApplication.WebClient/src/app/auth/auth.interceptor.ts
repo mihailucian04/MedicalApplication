e824e3134a4fa1651cfa38439baf5c00e9ebc6b2
CHIS Teofil <teofil.chis.07@gmail.com>
2018-05-31 00:00:12 +0300

    login/logout works

--- MedicalApplication.WebClient/src/app/auth/auth.interceptor.ts
+++ MedicalApplication.WebClient/src/app/auth/auth.interceptor.ts
@@ -1,38 +1,35 @@
-// import { Injectable, Inject, forwardRef } from '@angular/core';
-// import { HttpInterceptor, HttpHandler, HttpHeaderResponse,
-//      HttpSentEvent, HttpProgressEvent, HttpResponse, HttpUserEvent, HttpRequest, HttpEvent } from '@angular/common/http';
-// import { Observable } from 'rxjs/Observable';
-// import { Router } from '@angular/router';
+import { Injectable, Inject, forwardRef } from '@angular/core';
+import { HttpInterceptor, HttpHandler, HttpHeaderResponse,
+     HttpSentEvent, HttpProgressEvent, HttpResponse, HttpUserEvent, HttpRequest, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { Router } from '@angular/router';
 
 
-// @Injectable()
-// export class AuthInterceptor implements HttpInterceptor {
-//     constructor(@Inject(forwardRef(() => Router))private router: Router) {
-//     }
-//     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-//         if (req.headers.get('No-Auth') === 'True') {
-//             const cl = req.clone();
-//             return next.handle(cl);
-//         }
-//         if (localStorage.getItem('access_token') != null) {
-//             const clonedreq = req.clone({
-//                 headers: req.headers.set('Authorization', 'Bearer ' + localStorage.getItem('access_token'))
-//             });
-//             return next.handle(clonedreq)
-//             .do(
-//                 succ => {
-//                 },
-//                 err => {
-//                     // tslint:disable-next-line:no-debugger
-//                     debugger;
-//                     if (err.status === 401) {
-//                         this.router.navigateByUrl('/user/login');
-//                     }
-//                 }
-//             );
-//         } else {
-//             this.router.navigateByUrl('/user/login');
-//         }
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+    constructor(@Inject(forwardRef(() => Router))private router: Router) {
+    }
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        const idToken = localStorage.getItem('access_token');
+        if (idToken) {
+            const clonedreq = req.clone({
+                headers: req.headers.set('Authorization', 'Bearer ' + idToken)
+            });
+            return next.handle(clonedreq)
+            .do(
+                succ => {
+                },
+                err => {
+                    // tslint:disable-next-line:no-debugger
+                    debugger;
+                    if (err.status === 401) {
+                        this.router.navigateByUrl('/user/login');
+                    }
+                }
+            );
+        } else {
+            return next.handle(req);
+        }
 
-//     }
-// }
+    }
+}
